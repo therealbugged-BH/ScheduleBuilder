4d3bf90ae3041bd994991733db40e4a52436a0da
MaliaWilken4 <mawilken4@gmail.com>
2025-04-19 00:28:35 -0700

    added enter functionality to button

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 // This is how you get the dom element of this <button id="clicker">Update Counter</button>
 const clicker = document.getElementById("clicker");
-
-//arrays to store the tasks
-const taskDescriptions = [];
-const taskValues = [];
-
 // It returns an array so access the first instance of it with [0]
 const counter = document.getElementsByClassName("counter")[0];
-
 //for input
 const input = document.getElementById('taskInput')
 
@@ -17,18 +11,13 @@ console.log(clicker, counter);
 // .innerText returns the text associated with it
 console.log(clicker.innerText); //Prints "Update Counter"
 
-//Now, lets update the counter on button click!
-
-// whenever the clicker elemnt is clicked (via the "click"), it will run this function
-clicker.addEventListener("click", (e) => {
-  const newVal = Number(counter.innerText) + 1;
-  counter.innerText = newVal;
-});
+//arrays to store the tasks
+const taskDescriptions = [];
+const taskValues = [];
 
 // Add click event
-clicker.addEventListener('click', function() {
+function handleAddTask() {
   const inputValue = input.value.trim(); // Get and clean input
-
   // Split by comma
   const parts = inputValue.split(',');
 
@@ -56,7 +45,30 @@ clicker.addEventListener('click', function() {
   document.getElementById('displayArea').innerText = inputValue; // Display it in the div
   // Clear input
   input.value = '';
+};
+
+//Now, lets update the counter on button click!
+// whenever the clicker elemnt is clicked (via the "click"), it will run this function
+clicker.addEventListener("click", (e) => {
+  const newVal = Number(counter.innerText) + 1;
+  counter.innerText = newVal;
 });
+
+// Add task on button click
+clicker.addEventListener("click", handleAddTask);
+
+// Add task on Enter key press inside input
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault(); 
+    handleAddTask();
+    const newVal = Number(counter.innerText) + 1;
+    counter.innerText = newVal;
+  }
+});
+
+
+
 
 /*function displayText() {
   const input = document.getElementById('taskInput').value; // Get the input value
